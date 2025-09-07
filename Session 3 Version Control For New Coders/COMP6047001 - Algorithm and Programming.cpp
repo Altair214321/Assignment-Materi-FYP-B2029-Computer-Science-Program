32e8eb6e76ce71c7736e5b94d6f29709b6a41e3b
Matt <152750399+Altair214321@users.noreply.github.com>
2025-09-07 09:33:17 +0700

    Update COMP6047001 - Algorithm and Programming.cpp

---
+++ Session 3 Version Control For New Coders/COMP6047001 - Algorithm and Programming.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "John Doe";
+    string favoriteLanguage = "C++";
+    
+    cout << "My name: " << name << endl;
+    cout << "My favorite programming language: " << favoriteLanguage << endl;
+    
+    return 0;
+}
